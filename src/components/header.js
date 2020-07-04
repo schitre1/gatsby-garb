a383e413f3a1810ea911afbc94655090a5f73291
Chitre <schitre@paypal.com>
2020-07-04 14:46:55 -0700

    add logo image using normal image import

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,8 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import gatsbyIcon from "../images/gatsby-icon.png"
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
@@ -16,17 +18,21 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      {/* Title/Logo area */}
+      <span style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={gatsbyIcon} alt="Gatsby Garb logo" style={{ margin: '0 5px', width: '50px', border: '3px solid orange', borderRadius: '50%' }} />
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </span>
     </div>
   </header>
 )
