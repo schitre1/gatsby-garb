d67a3f561df78626971de12ba333276687607911
Chitre <schitre@paypal.com>
2020-07-05 00:36:26 -0700

    add netlify identity based product filtering

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,29 +1,58 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
 import Img from 'gatsby-image'
+import netlifyIdentity from 'netlify-identity-widget'
 
 import Layout from '../components/layout'
+import { render } from 'react-dom'
 
-const Products = ({ data: { allContentfulProduct } }) => (
-    <Layout>
-        <div>
-            {allContentfulProduct.edges.map(({ node: product }) => (
-                <div key={product.id}>
-                    <h2>Garb Products</h2>
-                    <Link to={`/products/${product.slug}`} style={{ textDecoration: 'none', color: '#551aa3' }}>
-                        <h3>
-                            {product.name} .{' '}<span style={{ fontSize: '1.2rem', fontWeight: 300, color: '#f60' }}>$ {product.price}</span>
-                        </h3>
-                    </Link>
-                    <Img
-                        style={{ maxWidth: 600 }}
-                        fluid={product.image.fluid}
-                    />
+class Products extends React.Component {
+    state = {
+        products: []
+    }
+    componentDidMount() {
+        this.getProducts()
+        netlifyIdentity.on('login', user => this.getProducts(user))
+        netlifyIdentity.on('logout', user => this.getProducts())
+    }
+
+    getProducts = (user) => {
+        console.log('Current user', user)
+        const allProducts = this.props.data.allContentfulProduct.edges
+        let filteredProducts = user ?
+            allProducts : allProducts.filter(({ node: product }) => {
+                console.log(product)
+                return !product.private
+            })
+
+        console.log(filteredProducts[0])
+        this.setState({ products: filteredProducts })
+    }
+
+    render() {
+        const { products } = this.state
+        return (
+            <Layout>
+                <div>
+                    {products.map(({ node: product }) => (
+                        <div key={product.id}>
+                            <h2>Garb Products</h2>
+                            <Link to={`/products/${product.slug}`} style={{ textDecoration: 'none', color: '#551aa3' }}>
+                                <h3>
+                                    {product.name} .{' '}<span style={{ fontSize: '1.2rem', fontWeight: 300, color: '#f60' }}>$ {product.price}</span>
+                                </h3>
+                            </Link>
+                            <Img
+                                style={{ maxWidth: 600 }}
+                                fluid={product.image.fluid}
+                            />
+                        </div>
+                    ))}
                 </div>
-            ))}
-        </div>
-    </Layout>
-)
+            </Layout>
+        )
+    }
+}
 
 export const PageQueryForProducts = graphql`
 query PageQueryForAllProducts {
@@ -34,6 +63,7 @@ query PageQueryForAllProducts {
               name
               slug
               price
+              private
               image {
                   fluid(maxWidth: 800){
                       ...GatsbyContentfulFluid_tracedSVG
